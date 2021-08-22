226cf2b8848a60d563c2c2bd91877bac10190da6
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-08-21 22:33:52 -0300

    feat react router principles

--- src/components/ConceptosBasicos.js
+++ src/components/ConceptosBasicos.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export const ConceptosBasicos = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Conceptos Básicos</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h3>Home</h3>
+            <p>Bienvenidos al tema de las rutas en react.</p>
+          </Route>
+          <Route exact path="/acerca">
+            <h3>Acerca</h3>
+          </Route>
+          <Route exact path="/contacto">
+            <h3>Contacto</h3>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
